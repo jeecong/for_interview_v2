67837b4419f23de185fc1590d5a8fc52747c226b
zhichongchong <zhichongchong@58.com>
2020-11-23 22:27:31 +0800

    init

---
+++ src/main/java/com/qiutong/work/controller/UserController.java
@@ -0,0 +1,17 @@
+package com.qiutong.work.controller;
+
+/**
+ * @Classname UserController
+ * @Description HOPE NO BUGS!
+ * @Date 2020/11/23 下午10:13
+ * @Created by Wayne Zhi
+ */
+@Controller
+@RequestMapping("user")
+public class UserController {
+
+    @RequestMapping("/login")
+    public String login(){
+
+    }
+}
